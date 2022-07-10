649b90e3bf66a5f81a9fa25e58876fd401b03359
Georg Sachs <gsx@protaisn.org>
2022-07-11 00:50:47 +0200

    Add a license

--- build.zig
+++ build.zig
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: AGPL-3.0-only
 const std = @import("std");
 
 pub fn build(b: *std.build.Builder) void {
--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: AGPL-3.0-only
 const std = @import("std");
 
 const Parameter = struct {
